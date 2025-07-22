fe6dd3e673ab930d0bbcdc898c853e098f8bd019
zakialawi02 <zaki.alawi22@gmail.com>
2025-07-22 20:06:47 +0700

    revert datepicker

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import { format, parse } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
@@ -11,19 +11,17 @@ type Mode = "month" | "date";
 type Props = {
   mode?: Mode;
   paramKey?: string;
-  className?: string;
 };
 
 export default function DatePickerInput({
   mode = "month",
   paramKey = "date",
-  className = "",
 }: Props) {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const formatString = mode === "month" ? "yyyy-MM" : "yyyy-MM-dd";
-  const displayFormat = mode === "month" ? "MMM yyyy" : "yyyy-MM-dd";
+
   const queryDate = searchParams.get(paramKey);
+  const formatString = mode === "month" ? "yyyy-MM" : "yyyy-MM-dd";
 
   const [selectedDate, setSelectedDate] = useState<Date>(() => {
     if (queryDate) {
@@ -35,21 +33,6 @@ export default function DatePickerInput({
     }
     return new Date();
   });
-
-  useEffect(() => {
-    const queryDate = searchParams.get(paramKey);
-    const newDate = queryDate
-      ? parse(queryDate, formatString, new Date())
-      : new Date();
-
-    setSelectedDate((prevDate) => {
-      const prevFormatted = format(prevDate, formatString);
-      const newFormatted = format(newDate, formatString);
-
-      // Hanya update jika berubah
-      return prevFormatted !== newFormatted ? newDate : prevDate;
-    });
-  }, [searchParams, paramKey, formatString]);
 
   const handleChange = (date: Date | null) => {
     if (!date) return;
@@ -64,10 +47,9 @@ export default function DatePickerInput({
     <DatePicker
       selected={selectedDate}
       onChange={handleChange}
-      dateFormat={displayFormat}
+      dateFormat={mode === "month" ? "MMM yyyy" : "yyyy-MM-dd"}
       showMonthYearPicker={mode === "month"}
-      showPopperArrow={false}
-      className={`bg-secondary-background border-2 border-black px-2 py-1 rounded-sm font-mono text-sm cursor-pointer focus:outline-none ${className}`}
+      className="bg-secondary-background border-2 border-black px-2 py-1 rounded-sm cursor-pointer"
       calendarClassName="neo-calendar"
     />
   );
